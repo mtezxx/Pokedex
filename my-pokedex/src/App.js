56936642dce61086b575ab25dada01836e18bc22
mtezxx <mkukucko@gmail.com>
2024-04-18 12:33:25 +0200

    setup + components

---
+++ my-pokedex/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {BrowserRoute as Router, Route, Switch, } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route></Route>
+        <Route></Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ my-pokedex/src/components/Pokedex.js
@@ -0,0 +1,9 @@
+import React,{useEffect,useState} from "react";
+import axios from 'axios';
+
+function Pokedex() {
+    const [pokemonList, setPokemonList] = useState([]);
+    const [nextUrl, setNextUrl] = useState("");
+    const [prevUrl, setPrevUrl] = useState("");
+}
+
